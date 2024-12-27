9c1c19d55ca72d9ee96aabfea27ddf57e30b9de7
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-12-27 13:36:07 +0300

    staged

--- src/normalize/__init__.py
+++ src/normalize/__init__.py
@@ -1,46 +1,8 @@
-NORMALIZE_KEYWORD: dict[str, str] = {
-    # latin to kiril
-    "a": "а",
-    "b": "б",
-    "c": "с",
-    "d": "д",
-    "e": "е",
-    "f": "ф",
-    "g": "г",
-    "h": "х",
-    "i": "и",
-    "j": "й",
-    "k": "к",
-    "l": "л",
-    "m": "м",
-    "n": "н",
-    "o": "о",
-    "p": "п",
-    "q": "қ",
-    "r": "р",
-    "s": "с",
-    "t": "т",
-    "u": "у",
-    "v": "в",
-    "w": "щ",
-    "x": "кс",
-    "y": "ы",
-    "z": "з",
+from normalize.settings import NORMALIZE_KEYWORD
 
-
-    # others
-    "ё": "е",
-    "а": "о",
-    "у": "и",
-
-
-    # ЛС
-    "личк": "лс",
-    "личных": "лс",
-}
 
 def normalize_string(t: str) -> str:
-    t = t.lower()
+    t = ''.join(t.lower().split())
 
     for base_symbol, norm_symbol in NORMALIZE_KEYWORD.items():
         t = t.replace(base_symbol, norm_symbol)
